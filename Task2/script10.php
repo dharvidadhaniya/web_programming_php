ac442eacd66c95196fcb407d4bf178e16baf88c4
dharvidadhaniya <dharvidadhaniya@gmail.com>
2024-09-28 10:55:20 +0530

    script10.php

--- Task2/script10.php
+++ Task2/script10.php
@@ -1,1 +1,10 @@
-
+<?php
+$nm="Dharvi";
+$age=20;
+$city="rajkot";
+$mobile_no=1234567890;
+echo "My Name is  ".$nm."<br>";
+echo "My Age is  ".$age."<br>";
+echo "My City is  ".$city."<br>";
+echo "My Mobile Number is ".$mobile_no;
+?>
